870137e0d8846762dfdd3eb1ac21c0562247da2f
Antony Nyagah <tony.m.nyagah@gmail.com>
2025-06-04 09:46:50 +0300

    feat: add Contact page component and update footer with dynamic links

---
+++ src/data/contactInfo.ts
@@ -0,0 +1,6 @@
+export const contacts = {
+  email: "tony.m.nyagah@gmail.com",
+  linkedin: "https://linkedin.com/in/antony-nyagah/",
+  github: "https://github.com/tony-nyagah",
+  twitter: "https://twitter.com/NyagahTony",
+};
